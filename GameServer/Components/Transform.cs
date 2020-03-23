d8285af92bb1f9a3dca8a521de003b9e2f50b0be
anzzy30 <anthony.brunel030@gmail.com>
2020-03-23 21:37:39 +0100

    component+transform update

---
+++ GameServer/Components/Transform.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace GameServer.Components
+{
+    class Transform
+    {
+
+        public Transform()
+        {
+
+        }
+
+            
+        void update(float dt)
+        {
+            //Normalement pas bugué ^:)^
+            float remaining_time = dt;
+            while (remaining_time > 0) {
+                if(remaining_time > times[0])
+                {
+                    remaining_time -= times[0];
+                    pos += velocities[0] * times[0] * speed;
+                    times.RemoveAt(0);
+                    velocities.RemoveAt(0);
+                    if (times.Count == 0)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    times[0] -= remaining_time;
+                    pos += velocities[0] * remaining_time * speed;
+                    if (times[0] < epsilon)
+                    {
+                        times.RemoveAt(0);
+                        velocities.RemoveAt(0);
+                    }
+                    break;
+                }
+            }
+        }
+
+
+        Vector2 pos;
+        Vector2 heading;
+
+        List<float> times; // [s]
+        List<Vector2> velocities;
+
+        float epsilon = 0.000001f;
+        float speed = 10000001f;//GL
+    }
+}
