692d810ecdb8e032f6b69d9c04130e78556e8344
Morgan Fouque <morganfouque@hotmail.Fr>
2020-03-24 21:30:05 +0100

    deleted packages

--- GameServer/Components/Transform.cs
+++ GameServer/Components/Transform.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace GameServer.Components
 {
